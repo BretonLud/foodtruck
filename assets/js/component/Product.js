2e508237a092c27a9ef559883db1b1dc1a62e1bc
Clergetf <florian.clerget@gmail.com>
2021-10-27 11:47:37 +0200

    couleur et agencement

--- assets/js/component/Product.js
+++ assets/js/component/Product.js
@@ -20,7 +20,7 @@ export default function Product(props) {
                         return <div key={product.name}className="product">
                             <img onClick={(e) => toggle(product)} className="small" src={product.image} alt={product.name}></img>
                             <h3>{product.name}</h3>
-                            <div>€{product.price}</div>
+                            <div>{product.price} €</div>
                             <div>
                                 <button className="btn btn-product" onClick={() => onAdd(product)}>Ajouter au panier</button>
                             </div>
